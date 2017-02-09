39d6436617affbd97c6fb7c487e1267333d3857d
Administrator <1207066631@qq.com>
2017-02-09 09:44:22 +0800

    请求

---
+++ app/src/main/java/bwie/day01/CustomTitleView.java
@@ -0,0 +1,102 @@
+package bwie.day01;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * 1.类的用途
+ * 2.@ renshaoshuai
+ * 3.@ 2017/2/8.
+ */
+public class CustomTitleView extends View {
+    private String mTitleText;
+    /**
+     * 文本的颜色
+     */
+    private int mTitleTextColor;
+    /**
+     * 文本的大小
+     */
+    private int mTitleTextSize;
+
+    private Rect mBound;
+    private Paint mPaint;
+
+    public CustomTitleView(Context context) {
+        this(context, null);
+    }
+
+    public CustomTitleView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView, defStyleAttr, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.CustomTitleView_titleText:
+                    mTitleText = a.getString(attr);
+                    break;
+                case R.styleable.CustomTitleView_titleTextColor:
+                    // 默认颜色设置为黑色
+                    mTitleTextColor = a.getColor(attr, Color.BLACK);
+                    break;
+                case R.styleable.CustomTitleView_titleTextSize:
+                    // 默认设置为16sp，TypeValue也可以把sp转化为px
+                    mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    break;
+
+            }
+
+        }
+        a.recycle();
+
+        /**
+         * 获得绘制文本的宽和高
+         */
+        mPaint = new Paint();
+        mPaint.setTextSize(mTitleTextSize);
+        // mPaint.setColor(mTitleTextColor);
+        mBound = new Rect();
+        mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int width;
+        int height ;
+        if(widthMode==MeasureSpec.EXACTLY){
+            width=widthSize;
+        }else{
+            mPaint.setTextSize(mTitleTextSize);
+//            mPaint.getTextBounds();
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+
+        mPaint.setColor(mTitleTextColor);
+        canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, mPaint);
+    }
+
+}
